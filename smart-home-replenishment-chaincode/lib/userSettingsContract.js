6e2368a5b77fc2ed47cd1539e9339fa714b73721
Jannik <altenhofer.j@web.de>
2021-11-02 22:14:35 +0100

    added consumer settings

--- smart-home-replenishment-chaincode/lib/userSettingsContract.js
+++ smart-home-replenishment-chaincode/lib/userSettingsContract.js
@@ -11,25 +11,46 @@ class UserSettingsContract extends Contract {
     /**
      * Sets the threshold that will trigger an ordering process when the
      * trasmitted sensor data for the specified iotdevice is overreaching.
-     * 
-     * @param {Object} ctx 
-     * @param {Number} threshold 
-     * @param {String} iotdevice 
-     */
-    async setReorderThreshold(ctx, threshold, iotdevice) {
-
-    }
-
-    /**
-     * Sets the product that will be reordered as soon as the threshold on the
-     * iotdevice is met.
+     * Also adds the product that will get reordered.
      * 
      * @param {*} ctx 
-     * @param {*} productId 
+     * @param {*} name 
+     * @param {*} org 
+     * @param {*} consumer 
+     * @param {*} threshold 
      * @param {*} iotdevice 
+     * @returns 
      */
-    async setReorderProduct(ctx, productId, iotdevice) {
+    async setReorderPolicy(ctx, name, org, consumer, threshold, iotdevice) {
+        console.log(`Starting to create reorder policy for consumer ${consumer} and iotdevice ${iotdevice}.`);
+        const productId = `product-${name}-${org}`;
+        const reorderPolicyId = `reorder-policy-${name}-${org}-${iotdevice}`;
 
+        const reorderPolicy = await ctx.stub.getState(reorderPolicyId)
+
+        if (!reorderPolicy || reorderPolicy.length === 0) {
+            console.info('Creating new reorder policy...');
+
+            reorderPolicy = {
+                id: reorderPolicyId,
+                productId: productId,
+                iotdevice: iotdevice,
+                consumer: consumer,
+                threshold: threshold
+            }
+
+            console.info(`Adding reorder policy with id ${newReorderPolicy.id} to the ledger:`, newReorderPolicy);
+            ctx.stub.putState(newReorderPolicy.id, Buffer.from(JSON.stringify(newReorderPolicy)));
+
+        } else {
+            console.info(`Found existing reorder policy with id ${reorderPolicyId}. 
+                Setting new threshold ${threshold} or iotdevice ${iotdevice}.`, reorderPolicy);
+
+            reorderPolicy.threshold = threshold || reorderPolicy.threshold;
+            reorderPolicy.iotdevice = iotdevice || reorderPolicy.iotdevice;
+        }
+
+        return JSON.stringify(reorderPolicy);
     }
 }
 
